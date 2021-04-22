db47099ac6f8f2ddfb85de38bae144d709a507df
Alvaro Santos <siruz_slipknot@hotmail.com>
2021-04-22 14:18:17 -0300

    trabalho-pratico-4

---
+++ persistencia-de-dados-versionamento-de-código-e-implantacao/trabalho-pratico-4/src/config/database.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+mongoose.Promise = global.Promise;
+
+const config = {
+  uri: 'mongodb+srv://root:root@cluster0.rvqkd.mongodb.net/account?retryWrites=true&w=majority',
+  options: { useNewUrlParser: true, useUnifiedTopology: true },
+};
+
+mongoose.connection.on('open', () => {
+  console.log('Successfully connection to database.');
+});
+
+mongoose.connection.on('error', () => {
+  throw new Error('Could not connect to MongoDB.');
+});
+
+export default {
+  connect: () => mongoose.connect(config.uri, config.options),
+};
